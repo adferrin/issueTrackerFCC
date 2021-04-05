df9addad172357b6d9c2f270cfc63333fc945e3c
adferrin <adferrin@icloud.com>
2021-04-05 13:41:33 -0700

    error found

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,7 +2,6 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
-const { delete } = require('../server');
 
 chai.use(chaiHttp);
 
